68c5ffebe7c225eb1f8109b67a5749152ca312b5
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2024-11-15 15:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function stringChop(str, size) {
+	 if(str<str.length || str==null){
+    return []
+  }
+  if(str==""){
+    return ""
+  }
+
+
+  let ans=[];
+let temp=""
+  for(let i=0;i<str.length;i++){
+    temp+=(str[i])
+    if(temp.length==size){
+      ans.push(temp)
+      temp=""
+    }
+  }
+  if(temp!=" "){
+    ans.push(temp)
+  }
+  return ans;
   // your code here
 }
 
